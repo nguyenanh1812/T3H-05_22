14cd050c77f084234940e61c92d618bffc3d4a1b
maihuy243 <maihuy243@gmail.com>
2022-08-08 21:58:19 +0700

    merge

--- src/pages/ex02/components/PackagesIncludes.jsx
+++ src/pages/ex02/components/PackagesIncludes.jsx
@@ -1,18 +1,14 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 export default function PackagesIncludes({ data }) {
-    return (
-        <div>
-            {data.map((item, index) => 
-            {
-              return 
-                <Fragment>
-                 <h4>Packges Includes</h4>
-                <h3>{data.name}</h3>
-                </Fragment>
-            }
-
-            )}
-        </div>
-    )
+  return (
+    <div>
+      {data.map((item, index) => (
+        <Fragment>
+          <h4>Packges Includes</h4>
+          <h3>{data.name}</h3>
+        </Fragment>
+      ))}
+    </div>
+  );
 }
--- src/pages/ex02/components/TotalMoney.jsx
+++ src/pages/ex02/components/TotalMoney.jsx
@@ -1,36 +1,13 @@
 import React from "react";
-
-export function TotalMoney({ data }) {
-  return (
-    <div class="col-md-6 d-flex py-5 px-5 justify-content-between">
-      {data.map((item, index) => (
-        <div key={index}>
-          <div class="col-md-5">
-            <p>$ {item.opt.price}/month</p>
-            <span>({item.opt.percentDiscount} %)</span>
-            <br />
-            <span>$ {item.opt.priceDiscount}/month</span>
-          </div>
-          <div class="col-md-5">
-            <p>- $ {item.opt.price - item.opt.priceDiscount}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-
-import React from 'react';
 import "../../../App.css";
-import { data } from "../../../constants";
+// import { data } from "../../../constants";
 
-function TotalMoney({data}) {
-  
+function TotalMoney({ data }) {
   return (
-
-    <div className='total-containt'>
+    <div className="total-containt">
       <div className="top">
         <div className="sub-total">Sub Total</div>
-        <div className='price'>$ 2.654</div>
+        <div className="price">$ 2.654</div>
         <div className="priceDiscout">-$2.233</div>
       </div>
       <div className="bottom">
@@ -38,10 +15,6 @@ function TotalMoney({data}) {
         <div className="resuilt-total">$2.654</div>
       </div>
     </div>
-
-    <div></div>
-
-  )
-
+  );
 }
 export default TotalMoney;
