02e8810cc974a2e1986ce161c30ffe92fc3cc6a1
thanhcong96199 <thanhcong96199@gmail.com>
2022-08-08 20:31:16 +0700

    define component in practice 02

---
+++ src/constants.js
@@ -0,0 +1,129 @@
+export const data = [
+  {
+    
+    name: "JavaScript",
+    img: 'https://www.freecodecamp.org/news/content/images/2021/06/javascriptfull.png',
+    opt: [
+      {
+        name: "30 days",
+        value: 30,
+        price: 319,
+        priceDiscount: 229,
+        percentDiscount: 28,
+      },
+      {
+        name: "90 days",
+        value: 90,
+        price: 186,
+        priceDiscount: 116,
+        percentDiscount: 37,
+      },
+      {
+        name: "180 days",
+        value: 180,
+        price: 134,
+        priceDiscount: 75,
+        percentDiscount: 44,
+      },
+      {
+        name: "365 days",
+        value: 365,
+        price: 106,
+        priceDiscount: 56,
+        percentDiscount: 49,
+      },
+    ],
+    course: "day",
+  },
+  {
+    name: "JavaScript",
+    img: 'https://www.freecodecamp.org/news/content/images/2021/06/javascriptfull.png',
+    opt: [
+      {
+        name: "30 days",
+        value: 30,
+        price: 319,
+        priceDiscount: 229,
+        percentDiscount: 28,
+      },
+      {
+        name: "90 days",
+        value: 90,
+        price: 186,
+        priceDiscount: 116,
+        percentDiscount: 37,
+      },
+      {
+        name: "180 days",
+        value: 180,
+        price: 134,
+        priceDiscount: 75,
+        percentDiscount: 44,
+      },
+      {
+        name: "365 days",
+        value: 365,
+        price: 106,
+        priceDiscount: 56,
+        percentDiscount: 49,
+      },
+    ],
+    course: "day",
+  },
+  {
+    name: "Question Interview",
+    img: 'https://www.freecodecamp.org/news/content/images/2021/06/javascriptfull.png',
+    opt: [
+      {
+        name: "1 Sessions",
+        value: 1,
+        price: 799,
+        priceDiscount: 499,
+        percentDiscount: 38,
+      },
+      {
+        name: "3 Sessions",
+        value: 3,
+        price: 2099,
+        priceDiscount: 1299,
+        percentDiscount: 38,
+      },
+      {
+        name: "5 Sessions",
+        value: 5,
+        price: 3299,
+        priceDiscount: 1999,
+        percentDiscount: 39,
+      },
+    ],
+    course: "session",
+  },
+  {
+    name: "MOCK INTERVIEWS",
+    img: 'https://www.freecodecamp.org/news/content/images/2021/06/javascriptfull.png',
+    opt: [
+      {
+        name: "1 Sessions",
+        value: 1,
+        price: 2099,
+        priceDiscount: 1299,
+        percentDiscount: 38,
+      },
+      {
+        name: "3 Sessions",
+        value: 3,
+        price: 2099,
+        priceDiscount: 1299,
+        percentDiscount: 38,
+      },
+      {
+        name: "5 Sessions",
+        value: 5,
+        price: 2099,
+        priceDiscount: 1299,
+        percentDiscount: 38,
+      },
+    ],
+    course: "session",
+  },
+];
---
+++ src/pages/ex02/components/Courses.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { data } from "../../../constants";
+
+const listCourse = data.map((el, index) => ({
+  ...el,
+  id: index + 1,
+}));
+
+export default function Courses() {
+  const [courses, setCourses] = useState([...listCourse]);
+  return (
+    <div>
+      {courses.map((el) => (
+        <ItemCourse key={el.id} inforCourse={el} />
+      ))}
+    </div>
+  );
+}
