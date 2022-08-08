af86484708fc500df09bcf9054d84c7f6294f23b
ttthuyen63 <ttthuyen6311@gmail.com>
2022-08-08 21:15:24 +0700

    TotalMoney

--- src/pages/ex02/components/TotalMoney.jsx
+++ src/pages/ex02/components/TotalMoney.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function TotalMoney() {
+export function TotalMoney({ data }) {
   return (
-    <div>TotalMoney</div>
-  )
+    <div class="col-md-6 d-flex py-5 px-5 justify-content-between">
+      {data.map((item, index) => (
+        <div key={index}>
+          <div class="col-md-5">
+            <p>$ {item.price}/month</p>
+            <span>({item.percentDiscount} %)</span>
+            <br />
+            <span>$ {item.priceDiscount}/month</span>
+          </div>
+          <div class="col-md-5">
+            <p>- $ {item.price - item.priceDiscount}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
